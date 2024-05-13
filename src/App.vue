ba55d2351e8c74c7359a530fa659c7ad5703ab07
Sandra Davies <dav.sandra1970@gmail.com>
2024-05-13 14:19:14 +0200

    Book-List-Button

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <LayoutHeader />
+  <BookList />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import LayoutHeader from "@/components/LayoutHeader.vue";
+import BookList from "@/components/BookList.vue";
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    LayoutHeader,
+    BookList,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/BookListRow.vue
@@ -0,0 +1,45 @@
+<template>
+  <tr class="table-item__table-row">
+    <td>{{ title }}</td>
+    <td>{{ isbn }}</td>
+    <td>
+      <BaseButton
+        :text="bookMarkedBtnText"
+        @btn-clicked="handleBookMarkButtonClick"
+        variant="secondary"
+      />
+    </td>
+  </tr>
+</template>
+
+<script>
+import BaseButton from "@/components/BaseButton.vue";
+export default {
+  components: {
+    BaseButton,
+  },
+  props: {
+    title: String,
+    isbn: String,
+    isBookmarked: Boolean,
+  },
+
+  computed: {
+    bookMarkedBtnText() {
+      return this.isBookmarked ? "Remove Bookmark" : "add Bookmark";
+    },
+  },
+
+  methods: {
+    handleBookMarkButtonClick() {
+      this.$emit("bookmark-clicked", this.isbn);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.table-item__table td {
+  padding: 12px 15px;
+}
+</style>
